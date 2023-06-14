55ac5a079523dc86f5397a5b8dff4f4dc79647fe
rodrigodias075 <diasmrodrigo5@gmail.com>
2023-06-14 17:51:52 -0300

    criando tabelas

--- webappbloom/Controllers/CompetenciaController.cs
+++ webappbloom/Controllers/CompetenciaController.cs
@@ -3,47 +3,49 @@ using WebAppBloom.Models;
 using WebAppBloom.ViewModels;
 using WebAppBloom.Contexts;
 namespace WebAppBloom.Controllers;
+public class CompetenciaController:Controller{
+    
+    private readonly AppDbContext _context;
 
+   public CompetenciaController(AppDbContext context){
+        _context = context;
+   } 
+   public IActionResult Index(){
 
-public class CompetenciaController : Controller
-{
+         Competencia competencia = new Competencia();
+         competencia.ColunaBloom = "Memorizar";
+         competencia.LinhaBloom = "Listar";
+         ViewData["titulo"] = "Compreender o funcionamento do Razor";
+         ViewData["tablebloom"] = competencia;
+         return View();
+   }
 
-
-private readonly AppDbContext _context;
-public CompetenciaController(AppDbContext context){
-_context = context;
-}
-   /*
-    public IActionResult Index()
-    {
-
-        Competencia competencia = new Competencia();
-        competencia.ColunaBloom = "Memorizar";
-        competencia.LinhaBloom = "Listar";
-        ViewData["titulo"] = "Compreender o funcionamento do razor";
-        ViewData["tablebloom"] = competencia;
-        return View();
-    }
-
-    public IActionResult RelatorioCompe()
-    {
-
-        var competencia = new Competencia()
-        {
-            ColunaBloom = "Teste Coluna",
-            LinhaBloom = "Teste Linha"
-        };
-
-        var viewModel = new DetalhesCompViewModel()
-        {
-
+   public IActionResult RelatorioCompe(){
+      
+      /*var competencia = new Competencia(){
+           ColunaBloom = "Teste Coluna",
+           LinhaBloom = "Teste Linha"
+      };
+      
+      var viewModel = new DetalhesCompViewModel(){
             Competencia = competencia,
             TituloPagina = "Página de Teste"
-        };
-        */
-        var competencias = _context.Competencias.ToList();
-        return View(viewModel);
+      };*/
+      var competencias = _context.Competencias.ToList();
+      return View();
 
+
+   }
+
+    public IActionResult Listar(){
+
+        var listaCompe = _context.Competencias.ToList();
+
+        var listaViewModel = new ListaCompetenciaViewModels{
+            Competencia = listaCompe
+        };
+        return View(listaViewModel);
     }
 
+   
 }
---
+++ webappbloom/ViewModels/ListaCompetenciaViewModel.cs
@@ -0,0 +1,9 @@
+
+using WebAppBloom.Models;
+public class ListaCompetenciaViewModels{
+
+public ICollection<Competencia> Competencia { get; set; } = new List<Competencia>();
+
+}
+
+
